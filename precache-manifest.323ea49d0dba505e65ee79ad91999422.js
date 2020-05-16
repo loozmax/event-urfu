de360f0a9333077b99455aaee4a4337966f1e72e
loozmax <62829571+loozmax@users.noreply.github.com>
2020-05-16 22:33:49 +0500

    Updates

---
+++ precache-manifest.323ea49d0dba505e65ee79ad91999422.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8e6f45854184d1de1f5f9e43a943ef",
+    "url": "/event-urfu/index.html"
+  },
+  {
+    "revision": "8834cbb1ca627c8dd207",
+    "url": "/event-urfu/static/css/main.f2325030.chunk.css"
+  },
+  {
+    "revision": "c8f24a78101b8bcef24e",
+    "url": "/event-urfu/static/js/2.596876ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/event-urfu/static/js/2.596876ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8834cbb1ca627c8dd207",
+    "url": "/event-urfu/static/js/main.e962f14b.chunk.js"
+  },
+  {
+    "revision": "8183f79ceb93d6fe2295",
+    "url": "/event-urfu/static/js/runtime-main.98fc6edb.js"
+  },
+  {
+    "revision": "578f3c561d42d34071527e7e2b532b2b",
+    "url": "/event-urfu/static/media/background.578f3c56.jpg"
+  },
+  {
+    "revision": "ed818fadd98c976113b270f7c708eb34",
+    "url": "/event-urfu/static/media/clock_1.ed818fad.svg"
+  },
+  {
+    "revision": "46490ccedb035fa02fb969f990616172",
+    "url": "/event-urfu/static/media/close-cross.46490cce.svg"
+  },
+  {
+    "revision": "b53d0c15b724e0f931c4c6dcaedd1eaa",
+    "url": "/event-urfu/static/media/internet.b53d0c15.svg"
+  },
+  {
+    "revision": "3ef8a1dfe60e7aa3a62c5ba96b526192",
+    "url": "/event-urfu/static/media/isLiked.3ef8a1df.svg"
+  },
+  {
+    "revision": "ac570b49bacc2cc33c95851cc840cb4f",
+    "url": "/event-urfu/static/media/isShared.ac570b49.svg"
+  },
+  {
+    "revision": "0d09859ad954a35c0d1ddb8c0faedd97",
+    "url": "/event-urfu/static/media/like.0d09859a.svg"
+  },
+  {
+    "revision": "26bee478f861eb4ca80e493255c257a6",
+    "url": "/event-urfu/static/media/location.26bee478.svg"
+  },
+  {
+    "revision": "448cdb4c2476858c89a4d84fc374671d",
+    "url": "/event-urfu/static/media/search.448cdb4c.svg"
+  },
+  {
+    "revision": "a808198053ea51d67a522b73be80c1d2",
+    "url": "/event-urfu/static/media/share.a8081980.svg"
+  },
+  {
+    "revision": "a37ada0e2d72e2a4a9f19fe0aa35b0ad",
+    "url": "/event-urfu/static/media/social.a37ada0e.svg"
+  },
+  {
+    "revision": "4ba940d8d5e09821e1ee08c27e367ed7",
+    "url": "/event-urfu/static/media/vk_blue.4ba940d8.svg"
+  },
+  {
+    "revision": "88ed95b5cad4ec6ca33dede00f179e5c",
+    "url": "/event-urfu/static/media/vk_white.88ed95b5.svg"
+  }
+]);
